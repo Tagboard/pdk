d9949c6472a01b6c1ee1228760e5fd327b907b78
Justin Brumley <justin@megahook.in>
2025-11-10 15:34:51 -0600

    chore(node): setup node-koa-ts example w/types

---
+++ examples/node-koa-typescript/types.ts
@@ -0,0 +1,83 @@
+/**
+ * Triggers are messages the Experience is configured to listen and
+ * react to. Each Trigger will be exposed as a button in Producer
+ * for the given Experience. When the Tagboard user clicks one of these
+ * buttons, a message will be posted to the Experience within an IFrame.
+ * How the Experience responds to these messages is up to the Partner.
+ * https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
+ */
+interface Trigger {
+  // Key is the string that we post to the IFrame via postMessage.
+  key: string
+
+  // Label is what is put on the button in Producer.
+  label: string
+}
+
+/**
+ * FieldOptions are the available options the Tagboard user
+ * will be able to choose from when setting up the Experience.
+ */
+interface FieldOption {
+  // Value is the actual data for the option. This is what will be sent to the Experience
+  // via the TemplateUrl.
+  value: string
+
+  // Label is how the Tagboard user will see the data in any dropdowns
+  // when editing the Experience.
+  label: string
+}
+
+/**
+ * Fields are the available overrides for any given Experience.
+ */
+interface Field {
+  // Key is a unique identifier for the Field.
+  // It is used to find where the data should go in the Experience's TemplateUrl.
+  // For example Key:player_id would replace all instances of "{{player_id}}" in
+  // Experience.TemplateUrl.
+  key: string
+
+  // DefaultValue controls what the initial value should be set to
+  // when the Tagboard user starts configuring the Experience.
+  // This is optional.
+  defaultValue: string
+
+  // Options are all the available field options the Tagboard user has to choose from.
+  options: FieldOption[]
+
+  // IsText controls whether the Field will display as a dropdown or plaintext input
+  // for the Tagboard users.
+  isText: boolean
+}
+
+/**
+ * Experiences are encompassing component that will be used to
+ * drive the HTML (IFrame) graphic element.
+ */
+interface Experience {
+  // Unique identifier for the Experience.
+  id: string
+
+  // Name of the Experience.
+  name: string
+
+  // Description of the Experience.
+  description: string
+
+  // URL used to render the Experience within an IFrame.
+  // Should be a template string with editable values replaced with template blocks.
+  // e.g. https://experience.tagboard.com/{{experience_id}}?player={{player_id}}
+  // where `{{experience_id}}` is Experience.ID and `{{player_id}}` would be replaced with
+  // the Field.Value of any Field where Field.Key is "player_id".
+  url: string
+
+  // Triggers that we will expose in Producer as buttons that
+  // post messages to the Experience Iframe.
+  triggers: Trigger[]
+
+  // The additional fields the Tagboard user will be able
+  // to configure on the Experience. This could be something like
+  // which "player" is shown, or what "match" is used for reference.
+  fields: Field[]
+}
