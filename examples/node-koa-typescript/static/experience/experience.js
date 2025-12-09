77cd2b820d63af15896837868b9c7dfa5fa3025c
Justin Brumley <justin@megahook.in>
2025-12-09 14:41:39 -0600

    fix(experience): add decodeURIComponent

--- examples/node-koa-typescript/static/experience/experience.js
+++ examples/node-koa-typescript/static/experience/experience.js
@@ -1,9 +1,9 @@
 const query = window.location.search.substr(1).split('&').reduce((memo, query) => ({
   ...memo,
-  [query.split('=')[0]]: query.split('=')?.[1] || true,
+  [query.split('=')[0]]: decodeURIComponent(query.split('=')?.[1]) || true,
 }), {});
 
-const experienceId = query.experienceId;
+const experienceId = query.experience_id;
 if (experienceId) {
   document.querySelector('#experienceId').textContent = experienceId;
 }
@@ -11,12 +11,11 @@ if (experienceId) {
 const fieldsEl = document.querySelector('#fields');
 
 Object.entries(query).forEach(([key, value]) => {
-  if (key === 'experienceId') {
+  if (key === 'experience_id') {
     return;
   }
 
   const el = document.createElement('h4');
   el.textContent = `${key}: ${value}`;
-
   fieldsEl.appendChild(el);
 });
